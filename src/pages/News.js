9c8e7d3571d0489f32bb0db6fda280f0538ff9ed
Connor Aucremanne <aucremanne@u.nus.edu>
2023-09-18 18:00:01 +0800

    fix pic

--- src/pages/News.js
+++ src/pages/News.js
@@ -5,6 +5,7 @@ import Container from '../components/Container';
 import './News.css'
 import NewsGrid from '../components/NewsGrid';
 
+import PierrePic from './../components/Website Individual Information/unknown.png'
 //import PierrePic from './../components/Website Individual Information/Pierre/image.jpg';
 import PegahPic from './../components/Website Individual Information/Pegah/image.jpg';
 import RiccardoPublication from './../components/Website Data/riccardo-publication.jpg'
