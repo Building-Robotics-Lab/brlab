6a2409abb608896b98d5d2b280d0a1e2e61de3b1
Building-Robotics-Lab <nus.buildingroboticslab@gmail.com>
2023-10-02 09:00:34 +0800

    update

--- src/pages/Publications.js
+++ src/pages/Publications.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import Container from '../components/Container';
 import './Publications.css'
 import { Link } from 'react-router-dom';
+import Select from 'react-select';
 
 import JournalData from '../components/Papers/JournalData';
 import ConferenceData from '../components/Papers/ConferenceData';
@@ -11,6 +12,98 @@ import PatentData from '../components/Papers/PatentData';
 import ThesesData from '../components/Papers/ThesesData';
 
 function Publications() {
+    const publication_types = [
+        { value: 0, label: 'All' },
+        { value: 1, label: 'Journal Article' },
+        { value: 2, label: 'Conference Paper' },
+        { value: 3, label: 'Patent' },
+        { value: 4, label: 'Thesis' },
+    ];
+    const [pubtype, setPubtype] = useState([publication_types[0]]);
+    const publicationType = (selectedOptions) => {
+        // If nothing is selected, select 'All'
+        if (!selectedOptions || selectedOptions.length === 0) {
+            setPubtype([publication_types[0]]);
+        }
+        // If the latest selected option is 'All' and other options are selected
+        else if (selectedOptions[selectedOptions.length - 1].value === 0 && selectedOptions.length > 1) {
+            setPubtype([publication_types[0]]);
+        }
+        // If the latest selected option is not 'All' and 'All' is among the selected options
+        else if (selectedOptions[selectedOptions.length - 1].value !== 0 && selectedOptions.some(option => option.value === 0)) {
+            setPubtype(selectedOptions.filter(option => option.value !== 0));
+        }
+        // Any other scenario
+        else {
+            setPubtype(selectedOptions);
+        }
+    }
+
+    const publication_year = [
+        { value: 'all', label: 'All' },
+        { value: '2023', label: '2023' },
+        { value: '2022', label: '2022' },
+        { value: '2021', label: '2021' },
+        { value: '2020', label: '2020' },
+        { value: '2019', label: '2019' },
+        { value: '2018', label: '2018' },
+        { value: '2017', label: '2017' },
+        { value: '2016', label: '2016' },
+        { value: '2015', label: '2015' },
+        { value: '2014', label: '2014' },
+        { value: '2013', label: '2013' },
+    ];
+    const [pubyear, setPubyear] = useState([publication_year[0]]);
+    const publicationYear = (selectedOptions) => {
+        // If nothing is selected, select 'All'
+        if (!selectedOptions || selectedOptions.length === 0) {
+            setPubyear([publication_year[0]]);
+        }
+        // If the latest selected option is 'All' and other options are selected
+        else if (selectedOptions[selectedOptions.length - 1].value === 'all' && selectedOptions.length > 1) {
+            setPubyear([publication_year[0]]);
+        }
+        // If the latest selected option is not 'All' and 'All' is among the selected options
+        else if (selectedOptions[selectedOptions.length - 1].value !== 'all' && selectedOptions.some(option => option.value === 'all')) {
+            const sortedOptions = selectedOptions.filter(option => option.value !== 'all').sort((a, b) => b.value - a.value);
+            setPubyear(sortedOptions);
+        }
+        // Any other scenario
+        else {
+            const sortedOptions = [...selectedOptions].sort((a, b) => b.value - a.value);
+            setPubyear(sortedOptions);
+        }
+    }
+
+    const publication_author = [
+        { value: 0, label: 'All' },
+        { value: 1, label: 'Ali Ghahramani' },
+        { value: 2, label: 'Riccardo Talami' },
+        { value: 3, label: 'CHEN Kai' },
+        { value: 4, label: 'Iqbal Shah' },
+        { value: 5, label: 'Connor Aucremanne' },
+        { value: 6, label: 'Ilyas Dawoodjee' },
+    ];
+    const [pubauthor, setPubauthor] = useState([publication_author[0]]);
+    const publicationAuthor = (selectedOptions) => {
+        // If nothing is selected, select 'All'
+        if (!selectedOptions || selectedOptions.length === 0) {
+            setPubauthor([publication_author[0]]);
+        }
+        // If the latest selected option is 'All' and other options are selected
+        else if (selectedOptions[selectedOptions.length - 1].value === 0 && selectedOptions.length > 1) {
+            setPubauthor([publication_author[0]]);
+        }
+        // If the latest selected option is not 'All' and 'All' is among the selected options
+        else if (selectedOptions[selectedOptions.length - 1].value !== 0 && selectedOptions.some(option => option.value === 0)) {
+            setPubauthor(selectedOptions.filter(option => option.value !== 0));
+        }
+        // Any other scenario
+        else {
+            setPubauthor(selectedOptions);
+        }
+    }
+
     return (
         <div className="Publications">
             <NavBar />
@@ -21,6 +114,20 @@ function Publications() {
                     <h3>For an up-to-date list:</h3>
                     <div className="HomeButtons">
                         <Link to="https://scholar.google.com/citations?user=9UACV-AAAAAJ&hl=en&oi=ao" target='_blank'><p id='JoinButton'>GOOGLE SCHOLAR</p></Link>
+                    </div>
+                </div>
+                <div className="publication_filter">
+                    <div className='publication_type'>
+                        <p>Type of Publication:</p>
+                        <Select isMulti name="type" options={publication_types} value={pubtype} defaultValue={publication_types[0]} onChange={publicationType} />
+                    </div>
+                    <div className='publication_year'>
+                        <p>Year Range:</p>
+                        <Select isMulti name="year" options={publication_year} value={pubyear} defaultValue={publication_year[0]} onChange={publicationYear} />
+                    </div>
+                    <div className='publication_type'>
+                        <p>Author:</p>
+                        <Select isMulti name="year" options={publication_author} value={pubauthor} defaultValue={publication_author[0]} onChange={publicationAuthor} />
                     </div>
                 </div>
             </Container>
