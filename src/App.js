908632d61b6fa23c0bad0b850bf17e804a2b2933
Connor Aucremanne <aucremanne@u.nus.edu>
2024-01-22 13:17:00 +0800

    code cleanup pt. II

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import News from './pages/News';
 import Publications from './pages/Publications';
 import Join_the_Lab from './pages/Join_the_Lab';
 import Collaborations from './pages/Collaborations';
-import Delete from './pages/Delete';
 
 import IndividualProfile from './pages/IndividualProfile';
 import IndividualPublication from './pages/IndividualPublication';
@@ -25,7 +24,6 @@ function App() {
         <Routes>
           <Route index element={<Home />} />
           <Route path='/research' element={<Research />} />
-          <Route path='/delete' element={<Delete />} />
           <Route path='/collaborations' element={<Collaborations />}></Route>
           <Route path='/comfortgpt' element={<ComfortGPT />} />
           <Route path='/otst' element={<OTST />} />
